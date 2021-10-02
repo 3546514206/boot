7189a12e7d60c1529d4b5e4e946acbc6503cb51b
杨海波 <3546514206@qq.com>
2021-10-02 10:54:51 +0800

    loader

---
+++ loader/src/main/java/edu/zjnu/LoaderApplication.java
@@ -0,0 +1,17 @@
+package edu.zjnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: LoaderApplication
+ * @author: 杨海波
+ * @date: 2021-10-01
+ **/
+@SpringBootApplication
+public class LoaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoaderApplication.class, args);
+    }
+}
